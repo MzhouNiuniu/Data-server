7c15dfe63b5396bdf236c9680099b967ba2191ba
chenkai <chenkai@oraro.net>
2019-04-02 10:59:47 +0800

    新增待审核企业接口

--- controller/order-controller.js
+++ controller/order-controller.js
@@ -4,6 +4,7 @@
 const API_URL = require('../config/ckApi');
 const httpUtils = require('../utils/httpUtils');
 var token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJsaWNlbnNlIjoib3Jhcm8gbGljZW5zZSIsInVzZXJfbmFtZSI6Ik1UTTVNVE16TXpNME5qTTZNR1ZrTVdFM016UXRZVGhsTXkwME1qUXlMVGcwTURBdE1UUXlNelprWm1Rd1ptVnoiLCJzY29wZSI6WyJzZXJ2ZXIiXSwicm9sZXMiOltdLCJleHAiOjE1NTQxNzU0NzMsImp0aSI6IjBkZWFiNWJiLWNlZDgtNDFjNi1iMTcwLWM5MTVkNjM4NjMwNyIsImNsaWVudF9pZCI6ImY1cjlkMDA3Yzc0MzQwYjY4MGM4NWNmZHNnZGY0M2tqZCIsInVzZXJuYW1lIjoiMTM5MTMzMzM0NjMifQ.AC7KXujtwHPGkLrbyTaDnAu0t98p8n2By5X2l4ZxjUAbcap_yvz58N47MlCCDPQxd3LBrfR-NbWEoth-4nTxvh0dJ4MRPpeVZS6bDVnzxbKmfrARRe_QyxN1IMJdA_pEDnc9E4jZaSTNR8_NkX3fKle-69xee2Cvnl54OIwu97HlLJRJ7UpKdVhm5Hzey_Jh4SCQ1sZShrTFtBR6J4dDh1_z4oDWfkd-kpiUPq8cCOF-8PeB2M6HSi1M5KGCBpSugsY_8RNZjndvAbSjw-rnipQBLj23MXO_ekien2GoYVJXXIt5bw2C9eXiozYcmzUrIT1G9Aw4mapEtxqjgqBhHQ'
+var errResponse = {code: 500, msg: '缺少必要参数'};
 
 class Order {
     constructor() {
@@ -16,9 +17,14 @@ class Order {
     * @apiParam {String} type 订单类型 'awaitPreviewOrder':待审核订单.
     * @apiParam {String} type 订单类型 'awaitDealOrder':待处理订单.
     * @apiParam {String} type 订单类型 'endingOrder':已结束订单.
+    * @apiParam {String} type 订单类型 'awaitPreviewCompany':待审核企业.
     * */
     async getOrderList(req, res, next) {
         let {pageSize, pageNum, type} = req.query;
+        if (!pageSize || !pageNum || !type) {
+            res.send(errResponse);
+            return false
+        }
         let url;
         switch (type) {
             case 'awaitPreviewOrder':
@@ -29,7 +35,10 @@ class Order {
                 break;
             case 'endingOrder':
                 url = API_URL.order.getFinishedOrder;
-                break
+                break;
+            case 'awaitPreviewCompany':
+                url = API_URL.company.getWaitAuditCompany;
+                break;
         }
         try {
             const result = await httpUtils.httpGet(url, {pageNum: pageNum, pageSize: pageSize}, token);
@@ -41,18 +50,31 @@ class Order {
 
     /*
     * 获取订单详情
-    * @apiParam {Number} orderId 订单id.
+    * @apiParam {Number} id 订单或企业id.
+    * @apiParam {String} type 类型：order订单，company企业.
     * */
     async getOrderDetail(req, res, next) {
-        let {orderId} = req.query;
-        if (!orderId) {
-            res.send({code: 500, msg: '缺少必要参数'});
+        let {id, type} = req.query;
+        if (!id || !type) {
+            res.send(errResponse);
             return false
         }
+        let url;
+        let params = {};
+        switch (type) {
+            case 'order':
+                url = API_URL.order.orderDetail;
+                params.orderId = id;
+                break;
+            case 'company':
+                url = API_URL.company.getWaitAuditCompanyInfo;
+                params.memberAccount = id;
+                break;
+        }
         try {
-            const result = await httpUtils.httpGet(API_URL.order.orderDetail, {orderId: orderId}, token);
+            const result = await httpUtils.httpGet(url, params, token);
             res.send(result)
-        }catch (e) {
+        } catch (e) {
             console.log(e)
         }
     }
@@ -63,14 +85,22 @@ class Order {
     * @apiParam {Number} id 订单id
     * @apiParam {String} type 订单类型 buyRequirementOrder 求购订单
     * @apiParam {String} type 订单类型 sellingProduct 出售商品
+    * @apiParam {String} type 订单类型 company 企业
     * */
     async checkOrder(req, res, next) {
         let {checkResult, id, type} = req.body;
         if (!checkResult || !id || !type) {
-            res.send({code: 500, msg: '缺少必要参数'})
+            res.send(errResponse)
             return false
         }
         let url;
+        let form = type == 'company' ? {
+            auditResult: checkResult,
+            memberAccount: id
+        } : {
+            checkResult: checkResult,
+            id: id
+        };
         switch (type) {
             case 'buyRequirementOrder':
                 url = API_URL.order.auditBuyRequirement;
@@ -78,10 +108,13 @@ class Order {
             case 'sellingProduct':
                 url = API_URL.order.auditSellProduct;
                 break;
+            case 'company':
+                url = API_URL.company.auditCompany;
+                break;
         }
-        const result = await httpUtils.httpPost(url, {checkResult: checkResult, id: id}, token);
+        const result = await httpUtils.httpPost(url, form, token);
         res.send(result)
     }
 }
 
-module.exports = new Order()
+module.exports = new Order();
--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,35 @@
 var express = require('express');
 var router = express.Router();
-const orderController = require('../controller/order-controller')
+const orderController = require('../controller/order-controller');
+/**
+ * @api {get} /order/getOrderList 获取订单或企业列表
+ * @apiName getOrderList
+ * @apiGroup order
+ *
+ * @apiParam {Number} pageSize 每页显示数量.
+ * @apiParam {Number} pageNum 页数.
+ * @apiParam {String} type 订单类型:awaitPreviewOrder待审核订单，awaitDealOrder待处理订单，endingOrder已结束订单，awaitPreviewCompany待审核企业.
+ */
 router.get('/getOrderList', orderController.getOrderList);
+
+/**
+ * @api {get} /order/getOrderDetail 获取订单获企业详情
+ * @apiName getOrderDetail
+ * @apiGroup order
+ *
+ * @apiParam {Number} id 订单或企业id.
+ * @apiParam {String} type 类型：order订单，company企业.
+ */
 router.get('/getOrderDetail', orderController.getOrderDetail);
+
+/**
+ * @api {post} /order/checkOrder 审核订单或企业
+ * @apiName checkOrder
+ * @apiGroup order
+ *
+ * @apiParam {Number} checkResult 审核结果:1通过，0不通过.
+ * @apiParam {Number} id 订单或企业id
+ * @apiParam {String} type 类型:buyRequirementOrder求购订单，sellingProduct出售商品，company企业
+ */
 router.post('/checkOrder', orderController.checkOrder);
 module.exports = router;
