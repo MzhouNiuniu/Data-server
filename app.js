5347e82dafb0ff0c441edfb53eaba39d564b81c3
zy <zhouyi@oraro.net>
2019-04-02 09:47:37 +0800

    解决跨域问题
    Change-Id: If52eeab53e9f947697319bcb977e1f09fe2427f1

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//跨域解决
+
+app.use('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+//res.header('Access-Control-Allow-Origin', 'http://www.baidu.com'); //这样写，只有www.baidu.com 可以访问。
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization,token');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
+    if (req.method == 'OPTIONS') {
+        res.send(200); // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
+    }
+    else {
+        next();
+    }
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
@@ -17,7 +33,7 @@ app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,19 +41,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,15 +1,16 @@
 define({
   "title": "cloud-server API",
   "url": "http://localhost:3030/v1",
-  "name": "node-middle",
-  "version": "0.0.0",
-  "description": "",
+  "name": "cloud-server",
+  "version": "1.0.0",
+  "description": "cloud-server项目API文档",
+  "forceLanguage": "zh-cn",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-03-29T01:37:10.235Z",
+    "time": "2019-04-02T01:45:56.822Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
