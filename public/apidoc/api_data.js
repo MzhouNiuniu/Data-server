107624628afc80a84d5ddbc41dd50d38ea54817d
zy <zhouyi@oraro.net>
2019-04-02 09:59:45 +0800

    新增apidoc
    Change-Id: I5fda75a165d7eac44c463feb85d475e3295a3e3e

--- public/apidoc/api_data.js
+++ public/apidoc/api_data.js
@@ -1,6 +1,43 @@
 define({ "api": [
   {
     "type": "get",
+    "url": "/selectData/getSelectData",
+    "title": "获取不需要入参的下拉框数据",
+    "name": "getSelectData",
+    "group": "SelectData",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "customsHasNoCompanyName",
+            "description": "<p>不带企业名称的客户列表.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "customsHasCompanyName",
+            "description": "<p>带企业名称的客户列表.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "provinceList",
+            "description": "<p>省份列表.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routes/selectData.js",
+    "groupTitle": "SelectData"
+  },
+  {
+    "type": "get",
     "url": "/user/:id",
     "title": "Request User information",
     "name": "GetUser",
